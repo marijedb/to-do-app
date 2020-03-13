b504ff4627938f3ea76e76789bdca4465d7abb76
Marije <49620465+marijedb@users.noreply.github.com>
2020-03-13 22:01:04 +0100

    Updates

---
+++ precache-manifest.41c16a2b48eb836711db8f44e7af7cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9991bf3b70606f46e769887ae9798105",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "1aa5c6110983a49f5b85",
+    "url": "/to-do-app/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "4a7bcc12579851fa47dd",
+    "url": "/to-do-app/static/js/2.f1ecee28.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/to-do-app/static/js/2.f1ecee28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa5c6110983a49f5b85",
+    "url": "/to-do-app/static/js/main.c3d4d322.chunk.js"
+  },
+  {
+    "revision": "bcb2cb86b912b58410b4",
+    "url": "/to-do-app/static/js/runtime-main.dd056bab.js"
+  }
+]);
