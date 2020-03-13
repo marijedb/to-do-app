03745ed72ded96297eed4377eeb7073eb8133f6e
Marije <49620465+marijedb@users.noreply.github.com>
2020-03-13 21:07:01 +0100

    First commit

---
+++ src/components/AddTodo.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class AddTodo extends Component {
+    state = {
+        searchfield: ''
+    }
+
+    // the event.target.title will make this function reusable for other fields. 
+    onChange = (event) => {
+        this.setState({ [event.target.title]: event.target.value});
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.addTodo(this.state.searchfield);
+        this.setState({ searchfield: ''})
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
+                <input 
+                    type="text" 
+                    title="searchfield" 
+                    placeholder="Add Todo..." 
+                    style={{flex: '10', padding: '5px'}} 
+                    value={this.state.searchfield}
+                    onChange={this.onChange}
+                />
+                <input 
+                    type="submit" 
+                    value="Submit" 
+                    className="btn" 
+                    style={{flex: '1'}} 
+                />
+            </form>
+        )
+    }
+}
+
+// AddTodo.propTypes = {
+//     todos: PropTypes.array.isRequired
+// }
+
+export default AddTodo;
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the Todo List v1.0.0</p>
+        </div>
+    )
+}
+
+export default About;
